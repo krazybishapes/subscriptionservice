d76e0b34796500cfb0a2d5f8a5d9593095b02f3b
krazybishapes <lpu.santosh@gmail.com>
2025-04-24 09:19:51 +0530

    initial commit

---
+++ src/main/java/com/finbox/subscrititionsvc/SubscrititionsvcApplication.java
@@ -0,0 +1,13 @@
+package com.finbox.subscrititionsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscrititionsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscrititionsvcApplication.class, args);
+	}
+
+}
